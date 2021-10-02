b98a30558ccc024abcdad1dd2f53aff7a0985024
Amélia Alice <alice.cardoso10@gmail.com>
2021-10-02 12:47:18 -0300

    :art: sketch 09

--- sketches/sketch-09.js
+++ sketches/sketch-09.js
@@ -1,40 +1,58 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
-const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [1080, 1080]
 };
 
-let x = 0;
-let y = 0;
-let spacing = 20;
+const PALETTE = [
+  ["#F25C69"],
+  ["#025373"],
+  ["#F2D785"],
+  ["#F2B33D"],
+];
+
+const initAxe = 5;
+const maxSize = 45;
+let x = initAxe;
+let y = initAxe;
+
+const circleRadius = [5, 20];
+const rectSize = [10, 35];
+
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = '#FFFFFF';
     context.fillRect(0, 0, width, height);
 
-    context.lineWidth = width * 0.01;
-    let maxObjects = width * spacing;
-    console.log(maxObjects)
-    for (let i = 0; i <= maxObjects; i++) {
+    let maxObjects = height * (height / maxSize);
+    for (let i = 0; i < maxObjects; i++) {
+      const color = random.pick(PALETTE);
 
-      if (random.range(1) < 0.5) {
-        context.beginPath();
-        context.rect(x, y, 20, 20);
-        context.stroke();
-      } else {
-        
+      if (random.range(1) < 0.8) {
+        if (random.range(1) < 0.5) {
+          const radius = random.range(circleRadius[0], circleRadius[1]);
+          context.beginPath();
+          context.arc(x + radius, y + radius, radius, 0, 2 * Math.PI);
+          context.fillStyle = color;
+          context.fill();
+        } else {
+          const size = random.range(rectSize[0], rectSize[1]);
+          context.beginPath();
+          context.fillStyle = color;
+          context.fillRect(x, y, size, size);
+          context.stroke();
+        }
       }
-      console.log(x)
-      console.log(y)
-      x += spacing;
+
+      x += maxSize;
       if (x > width) {
-        x = 0;
-        y += spacing;
+        x = initAxe;
+        y += maxSize;
       }
+
     }
-   
+
   };
 };
 
